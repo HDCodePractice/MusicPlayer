fd52a8fda838c7e5988bd8b2924b67962209e1c5
Danny Huang <huangdong@gmail.com>
2021-08-15 19:55:54 -0400

    减少BotCommand的提示
    fix #26

--- main.py
+++ main.py
@@ -52,10 +52,10 @@ bot.start()
 bot.send(
     functions.bots.SetBotCommands(
         commands=[
-            types.BotCommand(
-                command="start",
-                description="Check if bot alive//看一看机器人还是不是活着的"
-            ),
+            # types.BotCommand(
+            #     command="start",
+            #     description="Check if bot alive//看一看机器人还是不是活着的"
+            # ),
             types.BotCommand(
                 command="help",
                 description="Show the help message//显示帮助信息"
@@ -64,10 +64,10 @@ bot.send(
                 command="play",
                 description="Play a song. Syntax: /play <song name> //请用 /play 歌曲名 来点播歌曲"
             ),
-            types.BotCommand(
-                command="player",
-                description="Shows current playing song with controls//查看正在播放的歌曲"
-            ),
+            # types.BotCommand(
+            #     command="player",
+            #     description="Shows current playing song with controls//查看正在播放的歌曲"
+            # ),
             types.BotCommand(
                 command="playlist",
                 description="Shows the playlist//查看playlist"
@@ -76,46 +76,46 @@ bot.send(
                 command="skip",
                 description="Skip the current song (reserved for admin)//跳过当前的歌曲（管理员专用）"
             ),
-            types.BotCommand(
-                command="join",
-                description="Join VC (reserved for admin)//让我加入voice chat（管理员专用）"
-            ),
-            types.BotCommand(
-                command="leave",
-                description="Leave from VC (reserved for admin)//让我离开voice chat（管理员专用）"
-            ),
+            # types.BotCommand(
+            #     command="join",
+            #     description="Join VC (reserved for admin)//让我加入voice chat（管理员专用）"
+            # ),
+            # types.BotCommand(
+            #     command="leave",
+            #     description="Leave from VC (reserved for admin)//让我离开voice chat（管理员专用）"
+            # ),
             # types.BotCommand(
             #     command="vc",
             #     description="Check if VC is joined (reserved for admin)（管理员专用）"
             # ),
-            types.BotCommand(
-                command="stop",
-                description="Stops Playing (reserved for admin)//停止播放（管理员专用）"
-            ),
-            types.BotCommand(
-                command="radio",
-                description="Start radio/Live stream (reserved for admin)//开启收音机/直播（管理员专用）"
-            ),
-            types.BotCommand(
-                command="stopradio",
-                description="Stops radio/Livestream (reserved for admin)//停止收音机/直播（管理员专用）"
-            ),
-            types.BotCommand(
-                command="replay",
-                description="Replay from beginning (reserved for admin)//重新开始播放playlist（管理员专用）"
-            ),
+            # types.BotCommand(
+            #     command="stop",
+            #     description="Stops Playing (reserved for admin)//停止播放（管理员专用）"
+            # ),
+            # types.BotCommand(
+            #     command="radio",
+            #     description="Start radio/Live stream (reserved for admin)//开启收音机/直播（管理员专用）"
+            # ),
+            # types.BotCommand(
+            #     command="stopradio",
+            #     description="Stops radio/Livestream (reserved for admin)//停止收音机/直播（管理员专用）"
+            # ),
+            # types.BotCommand(
+            #     command="replay",
+            #     description="Replay from beginning (reserved for admin)//重新开始播放playlist（管理员专用）"
+            # ),
             types.BotCommand(
                 command="clean",
                 description="Cleans RAW files (reserved for admin)//清除缓存文件(管理员专用)"
             ),
-            types.BotCommand(
-                command="pause",
-                description="Pause the song (reserved for admin)//暂停歌曲（管理员专用）"
-            ),
-            types.BotCommand(
-                command="resume",
-                description="Resume the paused song (reserved for admin)//重新开始放歌曲（管理员专用）"
-            ),
+            # types.BotCommand(
+            #     command="pause",
+            #     description="Pause the song (reserved for admin)//暂停歌曲（管理员专用）"
+            # ),
+            # types.BotCommand(
+            #     command="resume",
+            #     description="Resume the paused song (reserved for admin)//重新开始放歌曲（管理员专用）"
+            # ),
             # types.BotCommand(
             #     command="mute",
             #     description="Mute in VC (reserved for admin)//静音（管理员专用）"
